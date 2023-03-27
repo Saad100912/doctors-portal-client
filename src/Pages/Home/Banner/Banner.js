b4b10ead2cca1f9f4ca311192ef7c1d2504c088e
Saad100912 <arsaad.2016@gmail.com>
2023-03-27 14:57:28 +0600

    home page banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+        <div className="bg-[url('https://i.ibb.co/r2SsD3L/bg.png')] bg-contain">
+            <div className="hero min-h-[550px]">
+                <div className="hero-content grid grid-cols-1 lg:grid-cols-2 flex-col lg:flex-row-reverse">
+                    <div className="order-1 lg:order-2 flex justify-center">
+                        <img
+                            src="https://i.ibb.co/DwJwRFd/chair.png"
+                            className="rounded-lg shadow-2xl"
+                            alt="Dentist Chair"
+                        />
+                    </div>
+                    <div className="order-2 lg:order-1">
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">
+                            Provident cupiditate voluptatem et in. Quaerat
+                            fugiat ut assumenda excepturi exercitationem quasi.
+                            In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary font-medium text-white">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
